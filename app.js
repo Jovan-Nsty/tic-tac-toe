8d8d37b0e9b264ca91b854b02a9c41b51c3fbcb9
Jovan-Nsty <jova.zyo@gmail.com>
2023-12-26 13:37:57 +0100

    Added functions to check for win/ties and wether to end the game based on found winner or a tie

--- app.js
+++ app.js
@@ -5,11 +5,58 @@ const gameBoard = (() => {
     [null, null, null],
   ];
 
+  let gameActive = true;
+
+  const gameOver = () => {
+    gameActive = false;
+    console.log('Game Over!');
+  }
+
   const displayGameBoard = () => gameBoard;
 
-  const placeSymbol = (playerSymbol, row, col) => {
-    if (gameBoard[row][col] === null) {
-      gameBoard[row][col] = playerSymbol;
+  const placeSymbol = (playerName,playerSymbol, row, col) => {
+    if(gameActive) {
+      if (gameBoard[row][col] === null) {
+        gameBoard[row][col] = playerSymbol;
+      }
+      checkWin(playerName,playerSymbol);
+    }
+  };
+
+  const checkWin = (playerName,playerSymbol) => {
+    for (let i = 0; i < gameBoard.length; i++) {
+      for (let j = 0; j < gameBoard[i].length; j++) {
+        // check rows
+        if (gameBoard[j].every(row => row === playerSymbol)) {
+          console.log('Row win for ' + playerName);
+          gameOver();
+          break;
+        }
+        // check columns
+        if (gameBoard.every(column => column[j] === playerSymbol)) {
+          console.log('Column win for ' + playerName);
+          gameOver();
+          break;
+        }
+        // check diagonals
+        if (gameBoard[0][0] === playerSymbol && gameBoard[1][1] === playerSymbol && gameBoard[2][2] === playerSymbol) {
+          console.log('Left diagonal win for ' + playerName);
+          gameOver();
+          break;
+        }
+        if(gameBoard[0][2] === playerSymbol && gameBoard[1][1] === playerSymbol && gameBoard[2][0] === playerSymbol) {
+          console.log('right diagonal win for ' + playerName);
+          gameOver();
+          break;
+        }
+        // check ties
+        if(gameBoard.every(row => row.every(element => element !== null))) {
+          console.log(`It's a tie!`);
+          gameOver();
+          break;
+        }
+      }
+      break;
     }
   };
 
@@ -27,7 +74,7 @@ const createPlayer = (name, mark) => {
   const updatePlayersScore = () => playerScore++;
 
   const placeSymbolOnBoard = (row, col) => {
-    gameBoard.placeSymbol(playerSymbol, row, col);
+    gameBoard.placeSymbol(playerName,playerSymbol, row, col);
   };
 
   return {
@@ -39,5 +86,5 @@ const createPlayer = (name, mark) => {
   };
 };
 
-const playerOne = createPlayer('playerOne', 'X');
-const playerTwo = createPlayer('playerTwo', 'O');
+const playerOne = createPlayer('Player X', 'X');
+const playerTwo = createPlayer('Player O', 'O');
